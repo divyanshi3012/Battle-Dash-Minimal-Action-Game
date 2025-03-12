4234b129e0649b1c96382d4b43e3c64538a0d69e
divyanshi3012 <140187675+divyanshi3012@users.noreply.github.com>
2025-03-12 23:49:03 +0530

    Create Help.Card.tsx

---
+++ components/Help.Card.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HelpCard = () => {
+  return (
+    <div className="h-48 w-48 rounded-full overflow-hidden z-10 mx-auto">
+      <img
+        alt="battlefield-logo"
+        className="w-full h-full block mx-auto opacity-70"
+        src="https://user-images.githubusercontent.com/112806398/230717301-26fc31fc-8737-413a-a6b4-7d99d95d0138.png"
+      />
+    </div>
+  );
+};
+
+export default HelpCard;
