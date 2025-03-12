3cf2b85c828bbfe8622a8a498354ffc6b0f2076a
divyanshi3012 <140187675+divyanshi3012@users.noreply.github.com>
2025-03-12 23:50:12 +0530

    Create Home.Card.tsx

---
+++ components/Home.Card.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HomeCard = () => {
+  return (
+    <div className="h-80 w-80 rounded-full overflow-hidden flex justify-center items-center">
+      <img
+        className="block h-full w-full object-cover"
+        src="https://user-images.githubusercontent.com/112806398/230717301-26fc31fc-8737-413a-a6b4-7d99d95d0138.png"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default HomeCard;
